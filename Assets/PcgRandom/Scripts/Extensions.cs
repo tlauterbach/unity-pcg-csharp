d508af5f3fc2c9b4e7480144b260cf7e937dd1a8
Terra Lauterbach <terra.lauterbach@gmail.com>
2020-03-30 15:39:49 -0500

    added collection manipulation extension functions, added test for Shuffle algorithm

---
+++ Assets/PcgRandom/Scripts/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PcgRandom {
+
+	public static class Extensions {
+
+		public static void Shuffle<T>(this List<T> list, Pcg randomizer) {
+			int n = list.Count;
+			while (n > 0) {
+				int rand = Convert.ToInt32(randomizer.Range32(Convert.ToUInt32(n--)));
+				T item = list[rand];
+				list[rand] = list[n];
+				list[n] = item;
+			}
+		}
+		public static void Shuffle<T>(this T[] array, Pcg randomizer) {
+			int n = array.Length;
+			while (n > 0) {
+				int rand = Convert.ToInt32(randomizer.Range32(Convert.ToUInt32(n--)));
+				T item = array[rand];
+				array[rand] = array[n];
+				array[n] = item;
+			}
+		}
+		
+		public static T Choose<T>(this List<T> list, Pcg randomizer) {
+			return list[Convert.ToInt32(randomizer.Range32(Convert.ToUInt32(list.Count)))];
+		}
+		public static T Choose<T>(this T[] array, Pcg randomizer) {
+			return array[Convert.ToInt32(randomizer.Range32(Convert.ToUInt32(array.Length)))];
+		}
+		public static T Take<T>(this List<T> list, Pcg randomizer) {
+			T item = Choose(list, randomizer);
+			list.Remove(item);
+			return item;
+		}
+
+	}
+
+}
+
---
+++ Assets/Tests/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PcgRandom {
+
+	public sealed class Test : MonoBehaviour {
+
+		private void Awake() {
+			List<int> list = new List<int>() {
+				8, 3, -2, 39, 1290, 199, -29, 55, 1, 0
+			};
+			Pcg randomizer = new Pcg();
+			list.Shuffle(randomizer);
+			StringBuilder builder = new StringBuilder();
+			builder.Append("Output:");
+			foreach (int item in list) {
+				builder.Append('\n').Append(item.ToString());
+			}
+			Debug.Log(builder.ToString());
+		}
+
+	}
+
+}
